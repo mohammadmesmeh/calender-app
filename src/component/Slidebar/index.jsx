65541f002737bcf4c67e53675e38f4546aeb3f46
mohammadmesmeh <mesmeh2004@gmail.com>
2026-05-11 18:10:24 +0300

    fix the ShineButton

--- src/component/Slidebar/index.jsx
+++ src/component/Slidebar/index.jsx
@@ -6,7 +6,7 @@ import { ShineButton } from "../Buttons/ShineButton"
 import { Calendar, ChartColumn, CalendarClock, ListChecks, LayoutDashboard, Settings, CalendarPlus } from 'lucide-react'
 export const Slidebar = () => {
     return (
-        <div className="slidebar-container w-[300px] h-dvh tracking-tight bg-gradient-to-r from-primary to-secondary shadow-md   py-5 px-5 flex flex-col   text-white sticky top-0   ">
+        <div className=" slidebar-container w-[300px] h-dvh tracking-tight bg-gradient-to-r from-primary to-secondary shadow-md   py-5 px-5 flex flex-col   text-white sticky top-0   ">
 
             <nav>
                 <ul className="flex flex-col gap-4 mt-4">
@@ -20,12 +20,17 @@ export const Slidebar = () => {
 
                 </ul>
             </nav>
-            <ShineButton className="mt-4 h-fit">
+            <ShineButton className="mt-5 h-fit">
                 <CalendarPlus />
                 Add Task
             </ShineButton>
-            <hr className="my-6 border-0 h-px bg-gradient-to-r from-transparent via-border/70 to-transparent" />
-            <UserProfile classNameIcon='bg-white text-primary ' className='bg-secondary p-4 rounded-lg text-white mt-4  ' />
+            <div className="flex-1 flex flex-col justify-start gap-3 mt-3">
+                <div className="relative mb-4"><div className="absolute my-6 h-px w-full bg-gradient-to-r from-transparent via-border/70 to-transparent z-10 "></div></div>
+
+                <UserProfile classNameIcon='bg-white text-primary ' className='bg-secondary p-4 rounded-lg text-white mt-4  ' />
+
+            </div>
+
 
         </div>
     )
