2a31a52519d2e3bc747d81a6c5e6b8e1f2b33677
mohammadmesmeh <mesmeh2004@gmail.com>
2026-07-19 15:20:01 +0300

    feat(area-step-chart): add Recharts AreaStepChart and integrate into Dashboard

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -6,6 +6,7 @@ import { UpcomingEvents } from "../../component/UpcomingEvents"
 import { TodaysTasks } from "../../component/Today'sTasks"
 import { NotesWidget } from '../../component/NotesWidget'
 import { ProductivityAnalytics } from '../../component/ProductivityAnalytics'
+import { AreaStepChart } from '../../component/AreaStepChart'
 import { InViewAnimation } from "../../component/Animated Freamer Motion/InViewAnimation"
 import { SidebarProvider, useSidebarContext } from "../../context/SidebarContext"
 
@@ -27,6 +28,7 @@ const DashboardContent = () => {
           <InViewAnimation><UpcomingEvents /></InViewAnimation>
           <InViewAnimation><TodaysTasks /></InViewAnimation>
           <InViewAnimation><NotesWidget /></InViewAnimation>
+          <InViewAnimation><AreaStepChart /></InViewAnimation>
           <InViewAnimation><ProductivityAnalytics /></InViewAnimation>
         </div>
       </div>
